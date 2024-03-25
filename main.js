12795369894674e6e7d40c58353d8d9c64ae567d
khaulanauman <khaulanauman@gmail.com>
2024-03-25 20:54:45 +0500

    added new grid logic

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 "use strict"
 const container = document.querySelector(".container");
-window.onload=createGrid;
-function createGrid(){
-    for(let i=1;i<=16;i++){
+const resetBtn = document.querySelector("#reset");
+const newBtn = document.querySelector("#new");
+resetBtn.addEventListener("click",resetGrid);
+newBtn.addEventListener("click",newGrid);
+/*default grid*/
+window.onload=createGrid(16);
+function createGrid(size){
+    for(let i=1;i<=size;i++){
         const row = document.createElement("div");
-        row.style.display="flex";
         container.appendChild(row);
-        for(let j=1;j<=16;j++){
+        for(let j=1;j<=size;j++){
             console.log("div no "+j+" of row "+i+" created")
             const box = document.createElement("div");
             box.style.width="30px";
@@ -19,4 +23,31 @@ function createGrid(){
            row.appendChild(box);
         }
     }
+}
+function resetGrid(){
+    const container = document.getElementsByClassName("container")[0];
+    while (container.firstChild) {
+    container.box.removeAttribute("style");
+    }
+}
+function clearGrid(){
+    const container = document.getElementsByClassName("container")[0];
+    while (container.firstChild) {
+    container.removeChild(container.firstChild);
+    }
+}
+function newGrid(){
+    let size=0;
+    /*making sure size is less than 100*/
+    while(true){
+        size= window.prompt("Enter the no. of boxes per row");
+        if(size<=100){
+            break;
+        }
+        else{
+            alert("Please enter a number below 100");
+        }
+    }
+    clearGrid();
+    createGrid(size);
 }
